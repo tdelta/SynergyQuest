d321d281007c9c1ee50081b5fac1fe659036c4c1
Marc Arnold <marcoliver.arnold@stud.tu-darmstadt.de>
2020-05-28 15:36:03 +0200

    Added player prefab, added player animations, implemented player movement, added player collision with tileset

---
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    Animator animator;
+
+
+    float vertical;
+    float horizontal;
+    Vector2 lookDirection = new Vector2(1,0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3.0f;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate ()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        } 
+
+        animator.SetFloat("Look x", lookDirection.x);
+        animator.SetFloat("Look y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
+        Vector2 newPosition = transform.position;
+        newPosition.x = newPosition.x + horizontal * speed * Time.deltaTime;
+        newPosition.y = newPosition.y + vertical * speed * Time.deltaTime;
+
+        transform.position = newPosition;
+    }
+
+}
