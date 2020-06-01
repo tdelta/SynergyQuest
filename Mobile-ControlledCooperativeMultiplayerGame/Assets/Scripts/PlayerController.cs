84850f15fc377f9403860dffa177daeaecb4fd13
Marc Arnold <marcoliver.arnold@stud.tu-darmstadt.de>
2020-06-01 19:38:10 +0200

    Implemented hit detection for player attacks

--- Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/PlayerController.cs
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,13 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     Vector2 lookDirection = new Vector2(1,0);
 
+    public Transform attackPointTop;
+    public Transform attackPointBottom;
+    public Transform attackPointLeft;
+    public Transform attackPointRight;
+
+    public float attackRadius;
+    bool attacking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,31 +32,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            attack();
+        }   
     }
 
     void FixedUpdate ()
     {
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
-
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        } 
-
-        animator.SetFloat("Look x", lookDirection.x);
-        animator.SetFloat("Look y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
-
-
-        Vector2 newPosition = transform.position;
-        newPosition.x = newPosition.x + horizontal * speed * Time.deltaTime;
-        newPosition.y = newPosition.y + vertical * speed * Time.deltaTime;
-
-        transform.position = newPosition;
+        if(attacking == false) {
+            move();
+        }
     }
 
     public Vector2 getPosition()
@@ -62,4 +54,40 @@ public class PlayerController : MonoBehaviour
         return 1; //TODO :)
     }
 
+    private void attack()
+    {
+        animator.SetTrigger("Attack");
+        StartCoroutine(attackCo());
+    }
+
+    private IEnumerator attackCo()
+    {
+        attacking = true;
+        yield return new WaitForSeconds(0.3f);
+        attacking = false;
+    }
+
+    private void move()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look x", lookDirection.x);
+        animator.SetFloat("Look y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
+        Vector2 newPosition = transform.position;
+        newPosition.x = newPosition.x + horizontal * speed * Time.deltaTime;
+        newPosition.y = newPosition.y + vertical * speed * Time.deltaTime;
+
+        transform.position = newPosition;
+    }
 }
---
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Ich habe einen Gegner getroffen");
+    }
+}
