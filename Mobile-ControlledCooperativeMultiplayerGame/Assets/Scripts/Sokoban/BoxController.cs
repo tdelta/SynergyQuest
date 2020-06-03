dca94a3aae590016e19a77ef5a5f7bee75bcfa76
Marc Arnold <marcoliver.arnold@stud.tu-darmstadt.de>
2020-06-03 22:18:01 +0200

    Zwischenstand: Erste Implementierung des Schalters

---
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/Sokoban/BoxController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+
+    public objectcolor color;
+
+    public objectcolor getColor(){
+        return color;
+    }
+
+}
---
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/Sokoban/SwitchController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+
+    bool pressed;
+
+    public Transform collisionPointCenter;
+    public float collisionPointRadius;
+
+    private Collider2D[] colliders;
+
+    public void Start()
+    {
+        pressed = false;
+
+    }
+
+    public void Update()
+    {
+        pressed = false;
+        colliders = Physics2D.OverlapCircleAll(collisionPointCenter.position, collisionPointRadius);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.gameObject.tag == "Box"){
+                pressed = true;
+            }
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        //Gizmos.DrawSphere(collisionPointCenter.position, collisionPointRadius);
+    }
+
+    public bool isPressed(){
+        return pressed;
+    }
+}
