84e3283abdc9fc6812f9d6f12710d21bc49fcd62
Anton Haubner <anton.haubner@outlook.de>
2020-06-04 23:57:27 +0200

    Change implementation of Player movement
    * now incorporates delta time since last physics frame so that movement
      speed does not depend on the refresh rate of the physics engine
    * Now using MovePosition instead of AddForce. Whether we leave this
      change is still to be discussed

--- Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/PlayerController.cs
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class PlayerController : EntityController
 {
     [SerializeField] private GameObject lifeGauge;
+    [SerializeField] private float speed = 3.0f; // units per second
     [SerializeField] private int maxHealthPoints = 5;
-
-    public float speed = 3.0f;
+    
     private int _healthPoints;
 
     Animator animator;
-    new Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2D;
 
-
-    float vertical;
-    float horizontal;
-    Vector2 lookDirection = new Vector2(1,0);
-    bool attacking = false;
+    private float _vertical;
+    private float _horizontal;
+    private Vector2 _lookDirection = new Vector2(1,0);
+    private bool _attacking = false;
+    
+    /**
+     * Caching animation property identifiers and triggers as hashes for better performance
+     */
+    private static readonly int LookXProperty = Animator.StringToHash("Look x");
+    private static readonly int LookYProperty = Animator.StringToHash("Look y");
+    private static readonly int SpeedProperty = Animator.StringToHash("Speed");
+    private static readonly int AttackTrigger = Animator.StringToHash("Attack");
 
     void Awake()
     {
@@ -35,18 +42,18 @@ public class PlayerController : EntityController
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            attack();
+            Attack();
         }   
     }
 
     void FixedUpdate ()
     {
-        if(attacking == false) {
-            move();
+        if(_attacking == false) {
+            Move();
         }
     }
 
-    public override void putDamage(int amount, Vector2 knockbackDirection)  {
+    public override void PutDamage(int amount, Vector2 knockbackDirection)  {
         var stopForce = -rigidbody2D.velocity * rigidbody2D.mass;
         rigidbody2D.AddForce(stopForce + knockbackFactor * amount * knockbackDirection, ForceMode2D.Impulse);
         ChangeHealth(-amount);
@@ -66,9 +73,13 @@ public class PlayerController : EntityController
         }
     }
 
+    /**
+     * Displays a bar of hearts (life gauge) relative to the player avatar
+     */
     private void DisplayLifeGauge()
     {
         var spriteBounds = this.GetComponent<SpriteRenderer>().bounds.size;
+        // Set relative position of the life gauge so that it appears slightly above the player character
         this.lifeGauge.transform.localPosition =
             new Vector3(
                 0,
@@ -79,45 +90,46 @@ public class PlayerController : EntityController
         this.lifeGauge.GetComponent<LifeGauge>().DrawLifeGauge(_healthPoints, maxHealthPoints);
     }
 
-    public Vector2 getPosition()
+    public Vector2 GetPosition()
     {
         return rigidbody2D.position;
     }
 
-    private void attack()
+    private void Attack()
     {
-        animator.SetTrigger("Attack");
-        StartCoroutine(attackCoroutine());
+        animator.SetTrigger(AttackTrigger);
+        StartCoroutine(AttackCoroutine());
     }
 
-    private IEnumerator attackCoroutine()
+    private IEnumerator AttackCoroutine()
     {
-        attacking = true;
+        _attacking = true;
         yield return new WaitForSeconds(0.3f);
-        attacking = false;
+        _attacking = false;
     }
 
-    private void move()
+    private void Move()
     {
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+        _horizontal = Input.GetAxis("Horizontal");
 
-        Vector2 move = new Vector2(horizontal, vertical);
+        // Scale movement speed by the input axis value and the passed time to get a delta which must be applied to the current position
+        Vector2 deltaPosition = new Vector2(
+            _horizontal,
+            _vertical
+        ) * (speed * Time.deltaTime);
 
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
+        if (!Mathf.Approximately(deltaPosition.x, 0.0f) || !Mathf.Approximately(deltaPosition.y, 0.0f)) {
+            _lookDirection.Set(deltaPosition.x, deltaPosition.y);
+            _lookDirection.Normalize();
         }
 
-        animator.SetFloat("Look x", lookDirection.x);
-        animator.SetFloat("Look y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
-
-
-        Vector2 newPosition = rigidbody2D.position;
-        newPosition.x = horizontal * speed;
-        newPosition.y = vertical * speed;
-
-        rigidbody2D.AddForce(newPosition);
+        animator.SetFloat(LookXProperty, _lookDirection.x);
+        animator.SetFloat(LookYProperty, _lookDirection.y);
+        animator.SetFloat(SpeedProperty, deltaPosition.magnitude);
+        
+        rigidbody2D.MovePosition(
+            rigidbody2D.position + deltaPosition
+        );
     }
 }
