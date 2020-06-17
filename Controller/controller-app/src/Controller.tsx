fcf37e4c3d26fcee25dec37be1d6785b3deafbf5
kerscher <martin_x@live.de>
2020-06-17 13:03:07 +0200

    Fixed css, added fullscreen toggle, using fscreen to hopefully work on all browsers

--- Controller/controller-app/src/Controller.tsx
+++ Controller/controller-app/src/Controller.tsx
@@ -2,7 +2,11 @@ import { Button, MenuAction, ControllerClient, ConnectFailureReason } from 'cont
 import React from 'react';
 import nipplejs, { JoystickManager, JoystickManagerOptions, EventData, JoystickOutputData } from 'nipplejs';
 import './Controller.css';
+import { ColorData } from './consts';
+import fscreen from 'fscreen';
 
+import { faCompress, faExpand } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export class Controller extends React.Component<ControllerProbs, ControllerState> {
 
@@ -19,6 +23,12 @@ export class Controller extends React.Component<ControllerProbs, ControllerState
     this.onJoystickMoved = this.onJoystickMoved.bind(this);
     this.onJoystickReleased = this.onJoystickReleased.bind(this);
     this.onButtonChanged = this.onButtonChanged.bind(this);
+    this.toggleFullscreen = this.toggleFullscreen.bind(this);
+    this.onFullscreenChange = this.onFullscreenChange.bind(this);
+
+    this.state = {
+      isFullscreen: false
+    };
   }
 
   onButtonChanged(button: Button, pressed: boolean) {
@@ -46,6 +56,32 @@ export class Controller extends React.Component<ControllerProbs, ControllerState
     this.client.setJoystickPosition(vertical, horizontal);
   }
 
+  toggleFullscreen() {
+    // Hacky solution: Type should be Promise<void> but sadly the type definitions of fscreen say something else ...
+    let fullscreenFn: any = undefined;
+    
+    if (! this.state.isFullscreen){
+        fullscreenFn = fscreen.requestFullscreenFunction(document.documentElement) as any;
+    } else {
+        fullscreenFn = fscreen.exitFullscreen as any;
+    }
+    fullscreenFn.call(document.documentElement)
+    .then(
+      // Only set the state, if the fullscreen request was successfull
+      () => this.setState({ isFullscreen: !this.state.isFullscreen })
+    )
+    .catch(
+      (error: Error) => console.error('Error while requesting fullscreen')
+    );
+  }
+
+  onFullscreenChange() {
+    this.setState({
+      // Evaluates to true if window is in fullscreen
+      isFullscreen: fscreen.fullscreenElement !== null
+    });
+  }
+
   componentDidMount() {
     // Create joystick on div "boob"
     var boob = this.boob.current!; // Boob definetly exists when componentDidMount is called
@@ -57,7 +93,8 @@ export class Controller extends React.Component<ControllerProbs, ControllerState
     manager.on('end',this.onJoystickReleased);
 
     // The titlebar is annoying on mobile, so we get rid of it
-    document.documentElement.requestFullscreen();
+    this.toggleFullscreen();
+    fscreen.onfullscreenchange = this.onFullscreenChange;
   }
 
   render() {
@@ -77,21 +114,36 @@ export class Controller extends React.Component<ControllerProbs, ControllerState
     // Return DOM elements
     return(
       <div className='container'>
-      <div className='rowContainer' style={{userSelect: 'none'}}>
-        <div id='boob' className='text' ref={this.boob}> 
-          <p id='boobText'> tap and drag to move </p>
+        <div className='rowContainer' style={{ userSelect: 'none' }}>
+          <div className='columnContainer'>
+            <div className='gameControls'>
+              <div id='boob' className='text' ref={this.boob}>
+                <p id='boobText'> tap and drag to move </p>
+              </div>
+              <div className='buttonColumn'>
+                <div className='rowContainer' style={{height:'10%'}}>
+                  <button
+                      className='colorIndicator text no-click'
+                      style={{ backgroundColor: playerColor.dark, borderColor: playerColor.light }} >
+                      {playerColor.name}
+                  </button>
+                  <div id='fullscreenButton' onClick={this.toggleFullscreen}>
+                    <FontAwesomeIcon icon={this.state.isFullscreen ? faCompress : faExpand} />
+                  </div>
+                </div>
+                {button('Attack', Button.Attack, '#E53935', '#EF5350')}
+                {button('Pull', Button.Pull, '#039BE5', '#29B6F6')}
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='columnContainer'>
-          {button('Attack', Button.Attack, '#E53935', '#EF5350')}
-          {button('Pull', Button.Pull, '#039BE5', '#29B6F6')}
-        </div>
-      </div>
       </div>
     );
   }
 }
 
 interface ControllerState {
+  isFullscreen: boolean;
 }
 
 interface ControllerProbs {
