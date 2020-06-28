466dfb6c6348dbdaca8b33d9283954e7dcbd85cc
Anton Haubner <anton.haubner@outlook.de>
2020-06-28 19:32:38 +0200

    Always activate debug mode when running game in Editor

--- Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/DebugSettings.cs
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/DebugSettings.cs
@@ -3,11 +3,16 @@ public static class DebugSettings
     /**
      * Indicates whether the game shall be run in debug mode.
      * For example, when in debug mode, spawners spawn newly connected controllers which did not connect via lobby.
-     * This variable must be manually adjusted whenever needed.
+     * This variable can be manually adjusted whenever needed.
+     * By default, it is set to true, if running the game in Editor mode
      *
      * See also `PlayerSpawner`
      */
-    private static bool _debugMode = false;
+    #if UNITY_EDITOR
+        private static bool _debugMode = true;
+    #else
+        private static bool _debugMode = false;
+    #endif
     public static bool DebugMode => _debugMode;
 
     /**
