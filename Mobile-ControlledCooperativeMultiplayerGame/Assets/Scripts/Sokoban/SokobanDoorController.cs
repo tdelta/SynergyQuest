ad324221ead05829904b3c3c04a9c2c1e50e4c25
Anton Haubner <anton.haubner@outlook.de>
2020-07-08 14:47:39 +0200

    Improve prefab brushes, convert Sokoban 2 for testing
    * Also, Sokoban doors now automatically retrieve references to all
      present switches, if they have not been set manually

--- Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/Sokoban/SokobanDoorController.cs
+++ Mobile-ControlledCooperativeMultiplayerGame/Assets/Scripts/Sokoban/SokobanDoorController.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SokobanDoorController : DoorController
 {
     [SerializeField] private SwitchController[] switches;
+
+    private void Awake()
+    {
+        // If no switches have been configured manually, try to find them all at runtime
+        if (!switches.Any())
+        {
+            switches = FindObjectsOfType<SwitchController>();
+        }
+    }
 
     private void OnEnable()
     {
