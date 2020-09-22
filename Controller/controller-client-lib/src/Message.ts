5069330998e5cd433486fdba04f12408f0803396
Anton Haubner <anton.haubner@outlook.de>
2020-09-22 19:30:37 +0200

    Add license header to typescript and css files

--- Controller/controller-client-lib/src/Message.ts
+++ Controller/controller-client-lib/src/Message.ts
@@ -1,3 +1,30 @@
+/**
+ * This file is part of the "Synergy Quest" game
+ * (github.com/tdelta/SynergyQuest).
+ *
+ * Copyright (c) 2020
+ *   Marc Arnold     (m_o_arnold@gmx.de)
+ *   Martin Kerscher (martin_x@live.de)
+ *   Jonas Belouadi  (jonas.belouadi@posteo.net)
+ *   Anton W Haubner (anton.haubner@outlook.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <https://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7 apply,
+ * see `LICENSE.md` at the root of this source code repository.
+ */
+
 import {
   Button,
   MenuAction,
